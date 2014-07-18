00302855f3843e1c2b86ac0b900a8d3128959c7e
Nick Novitski <nicknovitski@gmail.com>
2014-07-18 23:25:19 +0000

    Automatically start or attach tmux on ssh

--- files/tmux.sh
+++ files/tmux.sh
@@ -1,5 +1,5 @@
 # if not in a tmux session
-#if [ -z "$TMUX" ]; then
+if [ -z "$TMUX" ]; then
   # attach to the first running session or start one
-#  exec tmux -u attach
-#fi
+  exec tmux -u attach
+fi
