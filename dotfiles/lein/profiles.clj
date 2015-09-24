8f3a0c38a5c90f83fec15fd760c16c35475e4387
Nick Novitski <nicknovitski@gmail.com>
2015-09-24 20:11:51 +0000

    Leiningen caches dependencies
    Since the clojure container is run with LEIN_ROOT, by default it
    downloads jars to /root/.m2/repository.  Making the user-space default
    location explicit lets them pile up like you'd expect.
    
    This took me an astoundingly long time to figure out, and it _still_
    doesn't work for plugins!  Aaaah!!

--- dotfiles/lein/profiles.clj
+++ dotfiles/lein/profiles.clj
@@ -1,3 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.2"]
+{:user {:local-repo "/home/dev/.m2/repository"
+        :plugins [[cider/cider-nrepl "0.8.2"]
                   [jonase/eastwood "0.2.1"]
                   [lein-cljfmt "0.1.7"]]}}
