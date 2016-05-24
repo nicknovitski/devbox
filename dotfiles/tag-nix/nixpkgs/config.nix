0c5e71a097fa55ba977f3d9eca8f2de6982f9e1c
Nick Novitski <nicknovitski@gmail.com>
2016-05-24 01:20:30 +0000

    nix: copy tools from Dockerfile

--- dotfiles/tag-nix/nixpkgs/config.nix
+++ dotfiles/tag-nix/nixpkgs/config.nix
@@ -9,7 +9,9 @@
 
       paths = [
         awscli
+        bashCompletion
         ctags
+        direnv
         elmPackages.elm-compiler
         elmPackages.elm-make
         elmPackages.elm-package
@@ -17,14 +19,22 @@
         elmPackages.elm-repl
         emacs
         git
+        gitAndTools.hub
         global
+        gtypist
+        links
         neovim
         mosh
         nox
+        parallel
         rcm
-        readline
+        rlwrap
+        sdcv
+        silver-searcher
         tmux
         typespeed
+        weechat
+        wget
       ];
     };
   };
